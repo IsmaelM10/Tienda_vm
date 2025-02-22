ae9896170e6f223f04eb0178db6929c587cdab4c
ismae <ismae@Bulsara>
2025-02-21 19:44:48 -0600

    Semana 5 Finalizado el CRUD de categoria

--- src/main/java/com/tienda/service/CategoriaService.java
+++ src/main/java/com/tienda/service/CategoriaService.java
@@ -9,12 +9,34 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoriaService {
+
     @Autowired
     private CategoriaRepository categoriaRepository;
-    
-    @Transactional(readOnly=true)
-    public List<Categoria> getCategorias(boolean activos){
-        var lista=categoriaRepository.findAll();
+
+    @Transactional(readOnly = true)
+    public List<Categoria> getCategorias(boolean activos) {
+        var lista = categoriaRepository.findAll();
+        //Falta algo xd
         return lista;
+    }
+
+    //Metodos del CRUD Create Read Update Delete
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaRepository.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Transactional
+    public void delete(Categoria categoria) {
+        //Elimina el registro que contiene el id igual a lo pasado en categoria.getIdCategoria()
+        categoriaRepository.delete(categoria);
+    }
+
+    @Transactional
+    public void save(Categoria categoria) {
+        //Si categoria.IdCategoria esta vacio... se inserta un registro
+        //si categoria.idCategoria tiene algo... se modifica ese registro
+        
+        categoriaRepository.save(categoria);
     }
 }
